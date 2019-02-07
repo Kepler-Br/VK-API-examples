26bd74961289d60571e5ca54b580ca5daebbfb44
Barrett Kepler <45532267+Kepler-Br@users.noreply.github.com>
2019-02-07 12:25:55 +0300

    Misc helper tools

---
+++ misc.py
@@ -0,0 +1,10 @@
+import os
+
+
+def is_file_exists(path: str) -> bool:
+    return os.path.exists(path)
+
+
+def create_folder(path: str):
+    if not is_file_exists(path):
+        os.makedirs(path)
